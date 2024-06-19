de7762c8c705de16bda67cfdcf0efaa47e854427
Jamie Barnes <jamie_lb@live.com>
2024-06-19 08:29:59 -0500

    added distinct verse pickers

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,54 +1,135 @@
 import {Component, OnInit} from '@angular/core';
 import {BibleService} from "../services/bible.service";
 import {Verse} from "../model/verse";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {BibleVersion} from "../model/bible-version";
+import {Book} from "../model/book";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    NgIf,
-    FormsModule
-  ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    imports: [NgIf, FormsModule, NgForOf],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
 
-  verse: Verse | undefined;
-  chapterNumber: number = 1;
-  verseNumber: number = 26;
-  isBusy: boolean = false;
-  errorMessage: string | undefined;
+    versions: BibleVersion[] = [];
+    books: Book[] = [];
+    bookChapters: number[] = [];
+    chapterVerses: number[] = [];
+    selectedVersionCode: string | undefined;
+    selectedBookId: string | undefined;
+    selectedChapterNumber: string | undefined;
+    selectedVerseNumber: string | undefined;
+    verse: Verse | undefined;
+    isBusy: boolean = false;
+    errorMessage: string | undefined;
 
-  constructor(private service: BibleService) {
-  }
+    constructor(private service: BibleService) {
+    }
 
-  ngOnInit(): void {
-    this.loadVerseDetail();
-  }
+    ngOnInit(): void {
+        this.isBusy = true;
+        this.service.getAllVersions().subscribe({
+            next: (allVersions: BibleVersion[]) => {
+                this.versions = allVersions;
+                this.selectedVersionCode = this.versions[0].versionCode;
+                this.service.getAllBooks().subscribe({
+                    next: (allBooks: Book[]) => {
+                        this.books = allBooks;
+                        this.selectedBookId = (this.books[0].id || 0).toString();
+                        this.loadChapterNumbers();
+                        this.isBusy = false;
+                    }, error: (error) => this.handleError(error)
+                });
+            }, error: (error) => this.handleError(error)
+        });
+    }
 
-  loadVerseDetail(): void {
-    this.isBusy = true;
-    this.errorMessage = undefined;
-    this.verse = undefined;
-    this.service.getVerse('ESV', 1, this.chapterNumber, this.verseNumber).subscribe({
-      next: (verse: Verse) => {
-        this.verse = verse;
+    selectedVersionChanged(): void {
+        this.loadChapterNumbers();
+    }
+
+    private loadChapterNumbers(): void {
+        this.bookChapters = [];
+        this.chapterVerses = [];
+        this.verse = undefined;
+        this.selectedChapterNumber = undefined;
+        this.selectedVerseNumber = undefined;
+        if (this.isVersionAndBookSelected()) {
+            this.isBusy = true;
+            const versionCode: string = this.selectedVersionCode || '';
+            const bookId: number = +(this.selectedBookId || '0');
+            this.service.getBookChapters(versionCode, bookId).subscribe({
+                next: (chapters: number[]) => {
+                    this.bookChapters = chapters;
+                    this.selectedChapterNumber = chapters[0].toString();
+                    this.loadChapterVerses();
+                    this.isBusy = false;
+                }, error: (error) => this.handleError(error)
+            });
+        }
+    }
+
+    private isVersionAndBookSelected(): boolean {
+        return (this.selectedVersionCode || '').trim().length > 0 && (+(this.selectedBookId || '0')) > 0;
+    }
+
+    selectedBookChanged(): void {
+        this.loadChapterNumbers();
+    }
+
+    chapterNumberChanged(): void {
+        this.loadChapterVerses();
+    }
+
+    private loadChapterVerses(): void {
+        this.chapterVerses = [];
+        this.verse = undefined;
+        this.selectedVerseNumber = undefined;
+        if (this.isVersionAndBookSelected() && (+(this.selectedChapterNumber || '0')) > 0) {
+            this.isBusy = true;
+            const versionCode: string = this.selectedVersionCode || '';
+            const bookId: number = +(this.selectedBookId || '0');
+            const chapterNumber: number = +(this.selectedChapterNumber || '0');
+            this.service.getChapterVerses(versionCode, bookId, chapterNumber).subscribe({
+                next: (verseNumbers: number[]) => {
+                    this.chapterVerses = verseNumbers;
+                    this.selectedVerseNumber = verseNumbers[0].toString();
+                    this.loadSelectedVerse();
+                    this.isBusy = false;
+                }, error: (error) => this.handleError(error)
+            });
+        }
+    }
+
+    verseNumberChanged(): void {
+        this.loadSelectedVerse();
+    }
+
+    loadSelectedVerse(): void {
+        this.errorMessage = undefined;
+        this.verse = undefined;
+        if (this.isVersionAndBookSelected() && (+(this.selectedChapterNumber || '0')) > 0 && (+(this.selectedVerseNumber || '0')) > 0) {
+            this.isBusy = true;
+            const versionCode: string = this.selectedVersionCode || '';
+            const bookId: number = +(this.selectedBookId || '0');
+            const chapterNumber: number = +(this.selectedChapterNumber || '0');
+            const verseNumber: number = +(this.selectedVerseNumber || '0');
+            this.service.getVerse(versionCode, bookId, chapterNumber, verseNumber).subscribe({
+                next: (verse: Verse) => {
+                    this.verse = verse;
+                    this.isBusy = false;
+                }, error: (error) => this.handleError(error)
+            });
+        }
+    }
+
+    private handleError(error: any): void {
         this.isBusy = false;
-      }, error: (error) => this.handleError(error)
-    });
-  }
-
-  chapterNumberChanged(): void {
-    this.verseNumber = 1;
-    this.loadVerseDetail();
-  }
-
-  private handleError(error: any): void {
-    this.isBusy = false;
-    this.errorMessage = error.error;
-  }
+        this.errorMessage = error.error;
+    }
 
 }
--- src/app/services/bible.service.ts
+++ src/app/services/bible.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Verse} from "../model/verse";
+import {BibleVersion} from "../model/bible-version";
+import {Book} from "../model/book";
 
 @Injectable({providedIn: 'root'})
 export class BibleService {
@@ -9,6 +11,26 @@ export class BibleService {
     private readonly baseUrl: string = 'http://localhost:8080/bible-be/';
 
     constructor(private http: HttpClient) {
+    }
+
+    getAllVersions(): Observable<BibleVersion[]> {
+        const url: string = this.baseUrl + 'versions';
+        return this.http.get<BibleVersion[]>(url);
+    }
+
+    getAllBooks(): Observable<Book[]> {
+        const url: string = this.baseUrl + 'books';
+        return this.http.get<Book[]>(url);
+    }
+
+    getBookChapters(versionCode: string, bookId: number): Observable<number[]> {
+        const url: string = this.baseUrl + versionCode + '/chapters/' + bookId;
+        return this.http.get<number[]>(url);
+    }
+
+    getChapterVerses(versionCode: string, bookId: number, chapterNumber: number): Observable<number[]> {
+        const url: string = this.baseUrl + versionCode + '/verses/' + bookId + '/' + chapterNumber;
+        return this.http.get<number[]>(url);
     }
 
     getVerse(versionCode: string, bookId: number, chapterNumber: number, verseNumber: number): Observable<Verse> {
